ae2387d59ac999990707aff2cb2e852d6c6ec74b
irisnode <sd1602@srmist.edu.in>
2026-05-07 16:51:03 +0530

    Added UC3 implementation

---
+++ TicTacToe-feature-UC3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
+
+    }
+
+    static int getUserSlot() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter slot number (1-9): ");
+
+        int slot = scanner.nextInt();
+
+        return slot;
+    }
+}
